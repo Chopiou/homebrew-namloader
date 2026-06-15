9ab6a931bc87bb44714eb3066f0ed06bb8769020
Chopiou <97546502+Chopiou@users.noreply.github.com>
2026-06-15 21:29:58 +0000

    Update SHA256 for Namloader v0.0.8

--- Formula/namloader.rb
+++ Formula/namloader.rb
@@ -2,8 +2,8 @@ class Namloader < Formula
   desc "Namloader VST3 plugin - template reusable pour inference NAM"
   homepage "https://github.com/Chopiou/nam-vst3"
   url "https://github.com/Chopiou/nam-vst3/releases/download/v#{version}/Namloader-v#{version}-macos-universal.vst3.tar.gz"
-  sha256 "dab4f31e1515ca056748c369a72b74b8147b42ca17949d50e022508812987894"
-  version "0.0.7"
+  sha256 "09b7abbe5d593af238a2e38c4f5271fb237b98cca7048a5b9613f8cf4bb67063"
+  version "0.0.8"
   license "MIT"
 
   def install
